93e94ea68ac0b3b983cc83c8cefa28258f007735
Eirik Brandtzæg <eirikb@eirikb.no>
2021-09-07 20:44:54 +0200

    Pathifier does the things
    Except remove

--- test/pathifier.ts
+++ test/pathifier.ts
@@ -50,211 +50,144 @@ test('flat 3', t => {
   t.deepEqual(array, ['a', 'b', 'X', 'c', 'Z']);
 });
 
-// test('flat 2', t => {
-//   const array: any[] = [];
-//   const data = new Data();
-//
-//   const flatTransformer = new FlatTransformer(data);
-//   flatTransformer.toArray(array);
-//
-//   flatTransformer.addTo([0], 'a');
-//   t.deepEqual(array, ['a']);
-//   flatTransformer.addTo([2], 'd');
-//   t.deepEqual(array, ['a', 'd']);
-//   flatTransformer.addTo([1, 1], 'c');
-//   t.deepEqual(array, ['a', 'c', 'd']);
-//   flatTransformer.addTo([1, 0], 'b');
-//   t.deepEqual(array, ['a', 'b', 'c', 'd']);
-// });
-//
-// test('flat 3', t => {
-//   const array: any[] = [];
-//   const data = new Data();
-//
-//   const flatTransformer = new FlatTransformer(data);
-//   flatTransformer.toArray(array);
-//
-//   flatTransformer.addTo([1, 1], 'd');
-//   t.deepEqual(array, ['d']);
-//   flatTransformer.addTo([0, 1], 'c');
-//   t.deepEqual(array, ['c', 'd']);
-//   flatTransformer.addTo([0, 0], 'a');
-//   t.deepEqual(array, ['a', 'c', 'd']);
-//   flatTransformer.addTo([0, 1], 'b');
-//   t.deepEqual(array, ['a', 'b', 'c', 'd']);
-// });
-//
-// test('flat 4', t => {
-//   const array: any[] = [];
-//   const data = new Data();
-//
-//   const flatTransformer = new FlatTransformer(data);
-//   flatTransformer.toArray(array);
-//
-//   flatTransformer.addTo([2], 'c');
-//   t.deepEqual(array, ['c']);
-//   flatTransformer.addTo([1], 'b');
-//   t.deepEqual(array, ['b', 'c']);
-//   flatTransformer.addTo([3], 'd');
-//   t.deepEqual(array, ['b', 'c', 'd']);
-// });
-//
-// test('flat 5', t => {
-//   const array: any[] = [];
-//   const data = new Data();
-//
-//   const flatTransformer = new FlatTransformer(data);
-//   flatTransformer.toArray(array);
-//
-//   const b = new DataTransformer(data, 'b');
-//   const c = new DataTransformer(data, 'c');
-//   b.init();
-//   c.init();
-//
-//   flatTransformer.addTo([1, 0], b);
-//   flatTransformer.addTo([1, 1], c);
-//
-//   flatTransformer.addTo([0], 'a');
-//   t.deepEqual(array, ['a']);
-//   flatTransformer.addTo([2], 'd');
-//   t.deepEqual(array, ['a', 'd']);
-//
-//   data.set('b', 'b');
-//   t.deepEqual(array, ['a', 'b', 'd']);
-//   data.set('c', 'c');
-//   t.deepEqual(array, ['a', 'b', 'c', 'd']);
-// });
-//
-// function don(data: Data, path: string): BaseTransformer<any, any> {
-//   const a = new DataTransformer(data, path);
-//   a.root = a;
-//   return a;
-// }
-//
-// test('flat 6', t => {
-//   const array: any[] = [];
-//   const data = new Data();
-//
-//   const flatTransformer = new FlatTransformer(data);
-//   flatTransformer.toArray(array);
-//
-//   flatTransformer.addTo(
-//     [1],
-//     don(data, 'a.$').map(l => l + 's')
-//   );
-//
-//   flatTransformer.addTo([0], 'a');
-//   t.deepEqual(array, ['a']);
-//   flatTransformer.addTo([2], 'd');
-//   t.deepEqual(array, ['a', 'd']);
-//
-//   data.set('a', ['lol']);
-//   t.deepEqual(array, ['a', 'lols', 'd']);
-// });
-//
-// test('flat 7', t => {
-//   const array: any[] = [];
-//   const data = new Data();
-//
-//   const flatTransformer = new FlatTransformer(data);
-//   flatTransformer.toArray(array);
-//
-//   flatTransformer.addTo(
-//     [1],
-//     don(data, 'a.$').map(l => don(data, `b.${l}`))
-//   );
-//
-//   flatTransformer.addTo([0], 'a');
-//   t.deepEqual(array, ['a']);
-//   flatTransformer.addTo([2], 'd');
-//   t.deepEqual(array, ['a', 'd']);
-//
-//   data.set('b.lol', 'yeah');
-//   data.set('a', ['lol']);
-//   t.deepEqual(array, ['a', 'yeah', 'd']);
-// });
-//
-// test('flat 8', t => {
-//   const array: any[] = [];
-//   const data = new Data();
-//
-//   const flatTransformer = new FlatTransformer(data);
-//   flatTransformer.toArray(array);
-//
-//   flatTransformer.addTo([0], ['a', ['b', 'c']]);
-//   t.deepEqual(array, ['a', 'b', 'c']);
-// });
-//
-// test('flat 9', t => {
-//   const array: any[] = [];
-//   const data = new Data();
-//
-//   const flatTransformer = new FlatTransformer(data);
-//   flatTransformer.toArray(array);
-//
-//   flatTransformer.addTo([1], don(data, 'a'));
-//
-//   flatTransformer.addTo([0], 'a');
-//   t.deepEqual(array, ['a']);
-//   flatTransformer.addTo([2], 'd');
-//   t.deepEqual(array, ['a', 'd']);
-//
-//   data.set('a', 'yes');
-//   t.deepEqual(array, ['a', 'yes', 'd']);
-//   data.set('a', undefined);
-//   t.deepEqual(array, ['a', 'd']);
-// });
-//
-// test('flat 10', t => {
-//   const array: any[] = [];
-//   const data = new Data();
-//
-//   const flatTransformer = new FlatTransformer(data);
-//   flatTransformer.toArray(array);
-//
-//   flatTransformer.addTo([0], 'a');
-//
-//   flatTransformer.addTo(
-//     [1],
-//     // don(data, 'a.$').map(l => [l, don(data, 'b.$')])
-//     don(data, 'a.$').map(_ => don(data, 'b.$'))
-//   );
-//
-//   flatTransformer.addTo([2], 'd');
-//
-//   console.log('  >HERE WE GO<');
-//
-//   // flatTransformer.init();
-//
-//   data.set('b', ['X', 'Y']);
-//   // data.set('b.y', 'Y');
-//
-//   data.set('a', ['A', 'whatever this will not be printed :P']);
-//
-//   console.log('array', array);
-//   // data.set('a', ['A', 'B']);
-//
-//   // Expect: a, A, X, Y, B, X, Y
-//   // console.log(array);
-//   // t.deepEqual(array, ['a', 'yeah', 'd']);
-//
-//   // data.set('a', undefined);
-//   // t.deepEqual(array, ['a', 'd']);
-//
-//   flatTransformer.prant();
-//
-//   t.pass();
-// });
-//
-// test('it', t => {
-//   const data = new Data();
-//   don(data, 'a.$')
-//     .sort((a, b) => b.t.localeCompare(a.t))
-//     .slice(0, 1)
-//     .map(v => {
-//       console.log('v', v);
-//     })
-//     .root?.init();
-//   data.set('a', [{ t: 'a' }, { t: 'b' }, { t: 'c' }]);
-//   t.pass();
-// });
+test('flat 4', t => {
+  const array: any[] = [];
+  const data = new Data();
+
+  const f = new Pathifier(data, new ToArrayTransformer(data, array));
+
+  f.put(0, 'a');
+  t.deepEqual(array, ['a']);
+  f.put(2, 'd');
+  t.deepEqual(array, ['a', 'd']);
+  f.put(1, ['b', 'c']);
+  t.deepEqual(array, ['a', 'b', 'c', 'd']);
+});
+
+test('flat 5', t => {
+  const array: any[] = [];
+  const data = new Data();
+  const f = new Pathifier(data, new ToArrayTransformer(data, array));
+
+  f.put(0, 'a');
+  f.put(
+    1,
+    f.on('test.$').map(x => f.on(`tast.${x}`))
+  );
+
+  data.set('tast', { b: 'X', c: 'Y' });
+  data.set('test', ['b', 'c']);
+  t.deepEqual(array, ['a', 'X', 'Y']);
+  data.set('tast.c', 'Z');
+  t.deepEqual(array, ['a', 'X', 'Z']);
+});
+
+test('flat 6', t => {
+  const array: any[] = [];
+  const data = new Data();
+
+  const f = new Pathifier(data, new ToArrayTransformer(data, array));
+
+  f.put(
+    1,
+    f.on('a.$').map(l => l + 's')
+  );
+  t.deepEqual(array, []);
+
+  f.put(0, 'a');
+  t.deepEqual(array, ['a']);
+  f.put(2, 'd');
+  t.deepEqual(array, ['a', 'd']);
+  data.set('a', ['lol']);
+  t.deepEqual(array, ['a', 'lols', 'd']);
+});
+
+test('flat 7', t => {
+  const array: any[] = [];
+  const data = new Data();
+
+  const f = new Pathifier(data, new ToArrayTransformer(data, array));
+
+  f.put(
+    1,
+    f.on('a.$').map(l => f.on(`b.${l}`))
+  );
+  f.put(0, 'a');
+  t.deepEqual(array, ['a']);
+  f.put(2, 'd');
+  t.deepEqual(array, ['a', 'd']);
+
+  data.set('b.lol', 'yeah');
+  data.set('a', ['lol']);
+  t.deepEqual(array, ['a', 'yeah', 'd']);
+});
+
+test('flat 8', t => {
+  const array: any[] = [];
+  const data = new Data();
+
+  const f = new Pathifier(data, new ToArrayTransformer(data, array));
+
+  f.put(0, ['a', ['b', 'c']]);
+  t.deepEqual(array, ['a', 'b', 'c']);
+});
+
+test('flat 9', t => {
+  const array: any[] = [];
+  const data = new Data();
+
+  const f = new Pathifier(data, new ToArrayTransformer(data, array));
+
+  f.put(1, f.on('a'));
+
+  f.put(0, 'a');
+  t.deepEqual(array, ['a']);
+  f.put(2, 'd');
+  t.deepEqual(array, ['a', 'd']);
+
+  data.set('a', 'yes');
+  t.deepEqual(array, ['a', 'yes', 'd']);
+  data.set('a', undefined);
+  t.deepEqual(array, ['a', 'd']);
+});
+
+test('flat 10', t => {
+  const array: any[] = [];
+  const data = new Data();
+
+  const f = new Pathifier(data, new ToArrayTransformer(data, array));
+
+  f.put(0, 'a');
+
+  f.put(
+    1,
+    f.on('a.$').map(_ => f.on('b.$'))
+  );
+
+  f.put(2, 'd');
+
+  data.set('b', ['X', 'Y']);
+
+  data.set('a', ['A', 'whatever this will not be printed :P']);
+
+  t.deepEqual(array, ['a', 'X', 'Y', 'X', 'Y', 'd']);
+  data.set('a', undefined);
+  t.deepEqual(array, ['a', 'd']);
+});
+
+test('it', t => {
+  const array: any[] = [];
+  const data = new Data();
+  const f = new Pathifier(data, new ToArrayTransformer(data, array));
+
+  f.put(
+    0,
+    f
+      .on('a.$')
+      .sort((a, b) => b.t.localeCompare(a.t))
+      .slice(0, 2)
+      .map(v => v.t)
+  );
+  data.set('a', [{ t: 'a' }, { t: 'b' }, { t: 'c' }]);
+  t.deepEqual(array, ['c', 'b']);
+});
